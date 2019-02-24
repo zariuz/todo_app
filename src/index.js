77379c5f601ffa0fa7f773eb02913cd3c309924a
zariuz <nechetov@hotmail.com>
2019-02-24 13:04:26 +0300

    add css files and item-status-filter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import AppHeader from './components/app-header';
 import SearchPanel from './components/search-panel';
 import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
 
 const App = () => {
 
@@ -13,12 +15,16 @@ const App = () => {
   ]
 
   return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+      
       <TodoList todos={todoData} />
     </div>
-  )
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
